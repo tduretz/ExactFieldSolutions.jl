417a3cede9819443440f13913853b2503febf9db
tduretz <duretz@uni-frankfurt.de>
2024-10-01 08:55:24 +0200

    clean Poisson1D

--- examples/benchmarks/Benchmark_Poisson1D.jl
+++ examples/benchmarks/Benchmark_Poisson1D.jl
@@ -2,88 +2,6 @@ using Plots, SparseArrays, Symbolics, SparseDiffTools, Printf, ExactFieldSolutio
 import LinearAlgebra: norm
 import Statistics: mean
 
-function main_test(Δx, ncx, L)
-
-    # Parameters
-    x   = (min=-L/2, max=L/2)
-    xc  = LinRange(x.min+Δx/2., x.max-Δx/2., ncx)
-    xce = LinRange(x.min-Δx/2., x.max+Δx/2., ncx+2)
-    xv  = LinRange(x.min, x.max, ncx+1)
-    xve = LinRange(x.min-Δx, x.max+Δx, ncx+3)
-
-    # Allocations
-    T      = zeros(ncx+2)
-    b      = zeros(ncx+2)
-    k      =  ones(ncx+3)
-    dTdx   =  ones(ncx+3)
-    q      =  ones(ncx+1)
-    qa     =  ones(ncx+3)
-    dqdx   =  ones(ncx+2)
-    F_dTdx = zeros(ncx+1)
-    F_dqdx = zeros(ncx-0)
-    F      = zeros(ncx)
-    Ta     = zeros(ncx)
-
-    # Initial condition: Evaluate exact initial solution
-    for i in eachindex(T)
-        sol     = Poisson1D_VarCoeff([xce[i]] )
-        T[i]    = sol.u
-        b[i]    = sol.s
-        dqdx[i] = sol.s
-    end
-    for i in eachindex(k)
-        sol     = Poisson1D_VarCoeff([xve[i]] )
-        k[i]    = sol.β
-        dTdx[i] = sol.∇u[1]
-        qa[i]   = sol.q[1]
-    end
-
-    for i in eachindex(q)
-        sol    = Poisson1D_VarCoeff([xv[i]] )
-        q[i]   = sol.q[1]
-    end
-    
-    α = 1/22
-    a = 12/11
-    α = 0.
-    a = 1.0
-
-    # T[2:end-1] .= 0.
-
-    for iter=1:300000
-        @. F_dTdx = α*dTdx[1:end-2] + dTdx[2:end-1] + α*dTdx[3:end-0] - a*(T[2:end] - T[1:end-1])/Δx
-        @. F_dqdx = α*dqdx[1:end-2] + dqdx[2:end-1] + α*dqdx[3:end-0] - a*(q[2:end] - q[1:end-1])/Δx
-        @. F      = -dqdx[2:end-1] - b[2:end-1]
-        # @. q[2:end-1]      = -k[3:end-2]*dTdx[3:end-2]
-        @. q[1:end-0]     = -k[2:end-1]*dTdx[2:end-1]
-        @. dTdx[2:end-1] -= F_dTdx ./10
-        @. dqdx[2:end-1] -= F_dqdx ./10
-        @. T[2:end-1]    += F/10000
-        norm(F)<1e-11 ? break : nothing
-        mod(iter, 10000)==0 ?  println(norm(F), ' ', norm(F_dTdx), ' ', norm(F_dqdx)) : nothing
-    end
-
-    # Evaluate exact solution
-    for i in eachindex(Ta)
-        sol   = Poisson1D_VarCoeff([xc[i]] )
-        Ta[i] = sol.u
-    end
-
-    @show T[1]
-    @show Ta[1]
-
-    # p1=plot(xc, abs.(T[2:end-1].-Ta))
-    # p1=plot(xc, T[2:end-1])
-    # p1=plot!(xc, Ta)
-    # p1=plot(xv, qa[2:end-1])
-    # p1=plot!(xv, q)
-    p1=plot(xv, abs.(qa[2:end-1] .-q))
-    display(p1)
-
-    # Error
-    return mean(abs.(T[2:end-1] .- Ta))
-end
-
 function Residual_BackwardEuler!(F, T, b, k, Δx, x)
     ncx = length(F)
     bC, bW, bE = 0., 0., 0.
@@ -149,7 +67,7 @@ function main(Δx, ncx, L)
     for i in eachindex(T)
         sol   = Poisson1D_VarCoeff([xc[i]] )
         T[i] = sol.u
-        b[i] = sol.s
+        b[i] =-sol.s
     end
     for i in eachindex(k)
         sol   = Poisson1D_VarCoeff([xv[i]] )
@@ -199,7 +117,7 @@ function ConvergenceAnalysis()
     Δxv = L ./ Ncx
     ϵx  = zero(Δxv)
     for i in eachindex(Ncx)
-       ϵx[i] = main_test(Δxv[i], Ncx[i], L)
+       ϵx[i] = main(Δxv[i], Ncx[i], L)
     end
 
     @show ϵx
@@ -214,9 +132,3 @@ function ConvergenceAnalysis()
 end
 
 ConvergenceAnalysis()
-
-
-# L   = 2.
-# Ncx = 10 
-# Δx = L ./ Ncx
-# main_test(Δx, Ncx, L)
